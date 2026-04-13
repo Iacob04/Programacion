7ad279c52a36749f28e065ac0cb5ccecf90c8b81
Iacob04 <alexiacob2004@gmail.com>
2026-04-13 14:11:52 +0200

    clase hoy

--- bases/src/bases/BasesDeDatos.java
+++ bases/src/bases/BasesDeDatos.java
@@ -2,7 +2,9 @@ package bases;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class BasesDeDatos {
 
@@ -10,11 +12,19 @@ public class BasesDeDatos {
 		
 		String usuario = "admin";
 		String password = "1234";
-		String server = "jdbc:mysql://localhost:3306/sakila";
+		String server = "jdbc:mysql://localhost:3306/logistica_global";
 		
 		try {
 			Connection conexion;
 			conexion = DriverManager.getConnection(server,usuario,password);
+			System.out.println("Conexion realizada ");
+			Statement query = conexion.createStatement();
+			ResultSet resultado = query.executeQuery("SELECT * FROM almacenes");
+			resultado.afterLast();
+			while(resultado.previous()) {
+				System.out.printf("%d - Almacen: (%d), %s, %s, %s \n",resultado.getRow(), resultado.getInt("id"),resultado.getString("ciudad_ubicacion"), resultado.getString("nombre_sucursal"),resultado.getString("cod_almacen") );
+			}
+			
 			conexion.close();
 		}catch(SQLException e) {
 			System.out.println("Error: "+ e.getMessage());
